7c43d8e495fc2c7f8970a334cd939e63b934d9df
Salviano Ludgerio <salviano@fretebras.com.br>
2016-12-15 15:51:45 -0200

    minor

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -5,17 +5,17 @@ directory '/var/www/api' do
   action :create
 end
 
+deploy_revision '/var/www/api' do
+  symlink_before_migrate.clear
+  create_dirs_before_symlink.clear
+  purge_before_symlink.clear
+  symlinks.clear
+end
+
 deploy 'api laravel' do
   repo 'https://github.com/salvianoo/angular-node-seed.git'
   user 'www-data'
   group 'www-data'
   deploy_to '/var/www/api'
   action :deploy
-end
-
-deploy_revision '/var/www/api' do
-  symlink_before_migrate.clear
-  create_dirs_before_symlink.clear
-  purge_before_symlink.clear
-  symlinks.clear
 end
