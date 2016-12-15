9c4176eee0f830f2574f1d86bd5e9d050e3de503
Salviano Ludgerio <salviano@fretebras.com.br>
2016-12-15 17:22:45 -0200

    Minor

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -5,26 +5,25 @@ directory '/var/www/api' do
   action :create
 end
 
-ruby_block 'Printing variables' do
-  block do
-    application = params[:app]
-    deploy = params[:deploy_data]
-
-    puts "salviano Application data: #{application}"
-    puts "salviano Deploy data: #{deploy}"
-  end
-  action :run
-end
-
-# deploy_revision "/var/www/api" do
-#   repo node['deploy']['api_fretes']['scm']['repository']
-#   user 'www-data'
-#   keep_releases 10
-#   action :deploy
-#   migrate false
-#   symlink_before_migrate.clear
-#   create_dirs_before_symlink
-#   purge_before_symlink.clear
-#   symlinks.clear
-#   symlinks {}
+# ruby_block 'Printing variables' do
+#   block do
+#     application = params[:app]
+#     deploy = params[:deploy_data]
+#     puts "salviano Application data: #{application}"
+#     puts "salviano Deploy data: #{deploy}"
+#   end
+#   action :run
 # end
+
+deploy_revision "/var/www/api" do
+  repo node["deploy"]["appshortname"]["scm"]["repository"]
+  user 'www-data'
+  keep_releases 5
+  action :deploy
+  migrate false
+  symlink_before_migrate.clear
+  create_dirs_before_symlink
+  purge_before_symlink.clear
+  symlinks.clear
+  symlinks {}
+end
