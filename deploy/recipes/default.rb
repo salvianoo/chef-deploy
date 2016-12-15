d0a3bc0cd69c838b67c6ee39e68ad2f4f791f613
Salviano Ludgerio <salviano@fretebras.com.br>
2016-12-15 15:38:22 -0200

    Minor

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,3 +1,10 @@
+directory '/var/www/api' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  action :create
+end
+
 deploy 'api laravel' do
   repo 'https://github.com/salvianoo/angular-node-seed.git'
   user 'www-data'
