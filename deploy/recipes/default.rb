670be7f8155f0ffbf8dba9c09c40f16b1cdaa3b8
Salviano Ludgerio <salviano@fretebras.com.br>
2016-12-15 16:31:00 -0200

    Minor

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -5,15 +5,26 @@ directory '/var/www/api' do
   action :create
 end
 
-deploy_revision "/var/www/api" do
-  repo node['deploy']['api_fretes']['scm']['repository']
-  user 'www-data'
-  keep_releases 10
-  action :deploy
-  migrate false
-  symlink_before_migrate.clear
-  create_dirs_before_symlink
-  purge_before_symlink.clear
-  symlinks.clear
-  symlinks {}
+ruby_block 'Printing variables' do
+  block do
+    application = params[:app]
+    deploy = params[:deploy_data]
+
+    Chef::Log.debug("salviano Application data: #{application}")
+    Chef::Log.debug("salviano Deploy data: #{deploy}")
+  end
+  action :run
 end
+
+# deploy_revision "/var/www/api" do
+#   repo node['deploy']['api_fretes']['scm']['repository']
+#   user 'www-data'
+#   keep_releases 10
+#   action :deploy
+#   migrate false
+#   symlink_before_migrate.clear
+#   create_dirs_before_symlink
+#   purge_before_symlink.clear
+#   symlinks.clear
+#   symlinks {}
+# end
