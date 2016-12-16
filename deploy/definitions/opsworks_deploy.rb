8630b0a7e0efde31719e714e6a28b42091ab5dbb
Salviano Ludgerio <salviano@fretebras.com.br>
2016-12-16 15:56:09 -0200

    minor

--- deploy/definitions/opsworks_deploy.rb
+++
@@ -1,7 +0,0 @@
-define :opsworks_deploy do
-  application = params[:app]
-  deploy      = params[:deploy_data]
-
-  puts "--------- DEBUGGGG!!! ----------"
-  puts application, deploy
-end
--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,64 +1,14 @@
+include_recipe 'dependencies'
+
 node[:deploy].each do |application, deploy|
 
+  opsworks_deploy_user do
+    deploy_data deploy
+  end
+
   opsworks_deploy do
-    deploy_data '/var/www/api_laravel'
-    app 'salvianoo'
+    deploy_data deploy
+    app application
   end
 
 end
-
-# directory '/var/www/api' do
-#   owner 'www-data'
-#   group 'www-data'
-#   mode '0755'
-#   action :create
-# end
-
-# template "/tmp/.ssh/chef_ssh_deploy_wrapper.sh" do
-#   source "chef_ssh_deploy_wrapper.sh.erb"
-#   owner node[:base][:username]
-#   mode 0770
-# end
-
-# template "/home/#{node[:base][:username]}/.ssh/id_rsa.pub" do
-#   source "id_rsa.pub.erb"
-#   owner node[:base][:username]
-#   mode 0600
-# end
-
-# template "/home/#{node[:base][:username]}/.ssh/id_rsa" do
-#   source "id_rsa.erb"
-#   owner node[:base][:username]
-#   mode 0600
-# end
-
-# ruby_block 'Printing variables' do
-#   block do
-#     application = params[:app]
-#     deploy = params[:deploy_data]
-#     puts "salviano Application data: #{application}"
-#     puts "salviano Deploy data: #{deploy}"
-#   end
-#   action :run
-# end
-
-# node[:deploy].each do |application, deploy|
-#   deploy = node[:deploy][application]
-
-#   puts "salviano ---- #{deploy}"
-
-#   deploy_revision "/var/www/api" do
-#     repo deploy["scm"]["repository"]
-#     user 'www-data'
-#     keep_releases 5
-#     action :deploy
-#     migrate false
-#     ssh_wrapper deploy["scm"]["ssh_key"]
-#     symlink_before_migrate.clear
-#     create_dirs_before_symlink
-#     purge_before_symlink.clear
-#     symlinks.clear
-#     symlinks {}
-#   end
-
-# end
